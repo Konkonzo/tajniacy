97a6e3fc5989eb5f6c6efa701bf8087aaa74d21d
Konkonzo <128140298+Konkonzo@users.noreply.github.com>
2023-12-23 11:26:29 +0100

    Important update

--- wyswietlanie.py
+++ wyswietlanie.py
@@ -4,7 +4,6 @@ def wyswietlanie(los):
     img_test = cv2.imread(r"hasla/1.png")
     y,x,ch=img_test.shape
     los = los+1
-    print(los)
     holder = np.zeros((y,x,25))
     for id,i in enumerate(los):
         img = cv2.imread(r"hasla/"+str(i)+".png",cv2.IMREAD_GRAYSCALE)
@@ -14,3 +13,4 @@ def wyswietlanie(los):
         conected[:,:,wiersz] = np.concatenate((holder[:,:,5*wiersz],holder[:,:,5*wiersz+1],holder[:,:,5*wiersz+2],holder[:,:,5*wiersz+3],holder[:,:,5*wiersz+4]), axis=0)
     img_out=np.concatenate((conected[:,:,0],conected[:,:,1],conected[:,:,2],conected[:,:,3],conected[:,:,4]), axis=1)
     cv2.imwrite("plansza.jpg",img_out)
+    print("Stworzono losową planszę do gry!")
